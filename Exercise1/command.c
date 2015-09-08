4f59ff571328642db1472481c59f26bc54b84388
Sam Kreter <samkreter@gmail.com>
2015-09-08 14:20:21 -0500

    finished input error checking and function defs for command.c

--- Exercise1/command.c
+++ Exercise1/command.c
@@ -10,12 +10,28 @@
 
 
 	//TODO FUNCTION COMMENT
+
+/*
+ * PURPOSE: parse user input into the commands struct
+ * INPUTS:
+ *	input: the list of commands from the user
+ *  cmd: double pointer to the command struct that holds all the commands
+ * RETURN:
+ *  If no errors occurred  then true
+ *  else false for an error in the process.
+ *
+ **/
+
 bool parse_user_input (const char* input, Commands_t** cmd) {
-	
+
 	//TODO ERROR CHECK INCOMING PARAMETERS
+	if(!input || !cmd){
+		return false;
+	}
+	//####################################
 
 	char *string = strdup(input);
-	
+
 	*cmd = calloc (1,sizeof(Commands_t));
 	(*cmd)->cmds = calloc(MAX_CMD_COUNT,sizeof(char*));
 
@@ -27,7 +43,7 @@ bool parse_user_input (const char* input, Commands_t** cmd) {
 		if (!(*cmd)->cmds[i]) {
 			perror("Allocation Error\n");
 			return false;
-		}	
+		}
 		strncpy((*cmd)->cmds[i],token, strlen(token) + 1);
 		(*cmd)->num_cmds++;
 		token = strtok(NULL, " \n");
@@ -37,10 +53,23 @@ bool parse_user_input (const char* input, Commands_t** cmd) {
 }
 
 	//TODO FUNCTION COMMENT
+/*
+ * PURPOSE: free the memory that holds all the commands
+ * INPUTS:
+ *	cmd: double pointer to the struct that holds the commands
+ * RETURN:
+ *  void
+ *
+ **/
+
 void destroy_commands(Commands_t** cmd) {
 
 	//TODO ERROR CHECK INCOMING PARAMETERS
-	
+	if(!cmd){
+		return;
+	}
+	//####################################
+
 	for (int i = 0; i < (*cmd)->num_cmds; ++i) {
 		free((*cmd)->cmds[i]);
 	}
